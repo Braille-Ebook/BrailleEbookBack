7281874917aca1ec8d0026e2c5d32fcce08fce8d
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-10-13 18:15:07 +0900

    [FEAT] PDF 파일에서 텍스트 추출 및 클라이언트에게 전달

---
+++ src/services/pdfReader.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+//import { getDocument } from 'pdfjs-dist';
+
+const pdfjsLib: any = require('pdfjs-dist/build/pdf.js');
+const WORKER_PATH = path.join(
+    __dirname,
+    '..', // out of utils
+    '..', // out of src
+    'node_modules',
+    'pdfjs-dist',
+    'legacy', // <-- NOTE: Use the legacy directory
+    'build',
+    'pdf.worker.js'
+);
+pdfjsLib.GlobalWorkerOptions.workerSrc = WORKER_PATH;
+
+export const extractPdfText = async (pdfUrl: string, page: number) => {
+    const pdf = await pdfjsLib.getDocument(pdfUrl).promise;
+    const pdfPage = await pdf.getPage(page);
+    const textContent = await pdfPage.getTextContent();
+    const text = textContent.items.map((item: any) => item.str).join(' ');
+    console.log(pdf.numPages);
+    console.log(textContent.items.length);
+    return text;
+};
